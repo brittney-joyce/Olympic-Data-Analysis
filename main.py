22bd70f3780e0f191313c3a8f5e40e77b4a2c7aa
Ritter Gustave <rGustave5@gmail.com>
2018-03-27 16:54:56 -0700

    blind commit

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from pprint import pprint
 
 #Code Execution
 #note for large files this will take a while and will take a lot of scrolling
+#Defining a dictionary to keep our data
+data_dict = {
+    
+}
+
+
+
 #this code is used to traverse files and collect data
 with open('Data/sports/hockey/hockey_ticket_master_pg0.json', encoding="UTF-8") as cf:
     data = json.loads(cf.read())
