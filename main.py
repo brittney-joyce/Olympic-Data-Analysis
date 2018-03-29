25c8634045b7ca19b00f9c99771a040951c63f95
Ritter Gustave <rGustave5@gmail.com>
2018-03-29 12:31:12 -0700

    Pulling Olympic tweet statements

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ analyzer = SentimentIntensityAnalyzer()
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+sentiment_dict = {
+
+}
+
+for post in api.search("Sochi Olympics"):
+    data = json.dumps(post._json, sort_keys=True, indent=4, separators=(',', ': '))
+    data = json.loads(data)
+    print(data['text'])
