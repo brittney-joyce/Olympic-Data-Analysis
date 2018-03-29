5f923a075d0fd3a8733e562370e064b64f3ed396
Ritter Gustave <rGustave5@gmail.com>
2018-03-29 12:02:06 -0700

    Created basis for sentiment analysis

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #Import statements
-import os
+import tweepy
 import json
-import request
+from config import consumer_key, consumer_secret, access_token, access_token_secret
 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyzer = SentimentIntensityAnalyzer()
 
 #Code Execution
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
