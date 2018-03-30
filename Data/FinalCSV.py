991279fef815198f723b3316cf5dd6d42394b65a
tgomez <34220115+tgomez@users.noreply.github.com>
2018-03-29 18:31:43 -0700

    Add files via upload

---
+++ Data/FinalCSV.py
@@ -0,0 +1,18 @@
+#Import Dependencies
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Importing CSV's
+summer_df = pd.read_csv ("summer.csv", encoding = "ISO-8859-1")
+winter_df = pd.read_csv ("winter.csv", encoding = "ISO-8859-1")
+dictionary_df = pd.read_csv ("dictionary.csv", encoding = "ISO-8859-1")
+
+#Merge Summer and Winter Olympic Information
+SW_Merge = summer_df.append(winter_df)
+
+#Adding Country Name to Merged CSV
+
+SW_Merge['Country_Name'] = ""
