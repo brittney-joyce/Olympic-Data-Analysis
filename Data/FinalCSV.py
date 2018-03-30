4d9075f9e965a57ca88f7c445930ff6f36925cbe
tgomez <34220115+tgomez@users.noreply.github.com>
2018-03-29 19:16:54 -0700

    Update FinalCSV.py

--- Data/FinalCSV.py
+++ Data/FinalCSV.py
@@ -8,14 +8,9 @@ import seaborn as sns
 #Importing CSV's
 summer_df = pd.read_csv ("summer.csv", encoding = "ISO-8859-1")
 winter_df = pd.read_csv ("winter.csv", encoding = "ISO-8859-1")
-dictionary_df = pd.read_csv ("dictionary.csv", encoding = "ISO-8859-1")
 
 #Merge Summer and Winter Olympic Information
 SW_Merge = summer_df.append(winter_df)
 
-#Adding Country Name to Merged CSV
-
-SW_Merge['Country_Name'] = ""
-SW_Merge['GDP'] = ""
-SW_Merge['Population'] = ""
+SW_Merge.to_csv("Summer_and_Winter", encoding="utf-8", index=False)
 #Incomplete Just here for show
