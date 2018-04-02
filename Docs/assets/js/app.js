15d19822ddc3036558a487559d5fa878c09182cb
Ritter Gustave <rGustave5@gmail.com>
2018-04-01 19:27:55 -0700

    Adding hosting folder

---
+++ Docs/assets/js/app.js
@@ -0,0 +1,16 @@
+// window.onload = evt => {
+//   // Testing link
+//   console.log('System Ready');
+//
+//   // Execute code
+//   // let options = {
+//   //   responsiveThreshold: 0
+//   // }
+//   // let elem = document.querySelector('.parallax');
+//   // let instance = M.Parallax.init(elem, options);
+//   $('.parallax').parallax();
+// }
+$(document).ready(function(){
+  $('.parallax').parallax();
+  $('.materialboxed').materialbox();
+});
