a7c3b158ce16368413e7888b07aafa84436e8a48
Ritter Gustave <rGustave5@gmail.com>
2018-04-01 23:02:49 -0700

    Increased resolution on my graph

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,9 @@
 //   // let elem = document.querySelector('.parallax');
 //   // let instance = M.Parallax.init(elem, options);
 //   $('.parallax').parallax();
-// }
+// Materialize non jquery dependancy didn't work, soooo I got screwed using this code
+// Used the following instead
+
 $(document).ready(function(){
   $('.parallax').parallax();
   $('.materialboxed').materialbox();
