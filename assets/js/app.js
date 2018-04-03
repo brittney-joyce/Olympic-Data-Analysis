4693e06134d47f1f89a95648767672c6af7bb406
Ritter Gustave <rGustave5@gmail.com>
2018-04-03 00:47:52 -0700

    Switched out of jQuery dependancy

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,31 @@
-// window.onload = evt => {
-//   // Testing link
-//   console.log('System Ready');
-//
-//   // Execute code
-//   // let options = {
-//   //   responsiveThreshold: 0
-//   // }
-//   // let elem = document.querySelector('.parallax');
-//   // let instance = M.Parallax.init(elem, options);
-//   $('.parallax').parallax();
+window.onload = evt => {
+  // Testing link
+  console.log('System Ready');
+
+  // Execute code
+  let options = {
+    responsiveThreshold: 0
+  }
+
+  let elem = document.querySelector('.parallax');
+  let instance = M.Parallax.init(elem, options);
+
+  let scrollSpyElem = document.querySelector('.scrollspy')
+  M.ScrollSpy.init(scrollSpyElem, {
+    throttle: 3000
+  })
+
+  M.AutoInit();
+}
 // Materialize non jquery dependancy didn't work, soooo I got screwed using this code
 // Used the following instead
 
-$(document).ready(function(){
-  console.log("Go Swiss Hockey! #77 Gustave, Allons-y!");
-  $('.parallax').parallax();
-  $('.materialboxed').materialbox();
-  $('.scrollspy').scrollSpy();
-});
+
+
+// Working JQuery
+// $(document).ready(function(){
+//   console.log("Go Swiss Hockey! #77 Gustave, Allons-y!");
+//   $('.parallax').parallax();
+//   $('.materialboxed').materialbox();
+//   $('.scrollspy').scrollSpy();
+// });
